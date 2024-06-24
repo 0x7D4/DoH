9192b049f0fc33f6b0326a3758a037d3f1f891ca
Sumeet <111058193+0x7D4@users.noreply.github.com>
2024-06-24 18:20:41 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,13 @@
+import json
+import subprocess
+
+# Load the websites from the JSON file
+with open('ranked_domains.json') as f:
+    websites = json.load(f)
+
+# Loop through each entry in the list
+for website in websites:
+    domain = website['domain']
+    command = f'curl --doh-insecure --doh-url https://localhost/dns-query https://www.{domain} -v'
+    print(f'Running command: {command}')
+    subprocess.run(command, shell=True)
