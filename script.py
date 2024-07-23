16301318f344823d264b5f3c588ac8658911dc04
Sumeet <111058193+0x7D4@users.noreply.github.com>
2024-07-23 14:56:45 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,13 @@
 import json
 import subprocess
 
-# Load the websites from the JSON file
+# Load the JSON file
 with open('ranked_domains.json') as f:
     websites = json.load(f)
 
-# Loop through each entry in the list
+# Loop through each website and run the curl command
 for website in websites:
-    domain = website['domain']
-    command = f'curl --doh-insecure --doh-url https://10.0.2.15/dns-query https://www.{domain} -v'
-    print(f'Running command: {command}')
-    subprocess.run(command, shell=True)
+        domain = website['domain']
+        command=f"curl --doh-insecure --doh-url https://10.0.2.15/dns-query https://www.{domain} -v'
+        print(f"Running command: {command}")
+        subprocess.run(command, shell=True)
